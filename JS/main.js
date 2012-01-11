167c5959f8a7c74d75154bafe4fc3ea32e800487
Russell Carlin <recarlin@fullsail.com>
2012-01-11 18:03:16 -0500

    Quick update
    I added some event listeners and set up my functions. Working on a way
    to populate my selector dynamically.

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,33 @@
-// Template
-// Author: Russell Carlin
-// Created for: SDI Online
+// Russell Carlin
+// VFW 1/12
+// Project 2
+window.addEventListener("DOMContentLoaded"), function (){
+    function ge(id){
+        var e = document.getElementById(id);
+        return e;
+    };
+    function runeSelection(){
+        var runeType = 
+    };
+    function showRunes(){
+        
+    };
+    function clearRunes(){
+        
+    };
+    function addRunes(){
+        
+    };
+    var sr = ge("show");
+    sr.addEventListener("click", showRunes);
+    var cr = ge("clear");
+    cr.addEventListener("click", clearRunes);
+    var ar = ge("add");
+    ar.addEventListener("click", addRunes);
+    var runes = {
+        mark: ["Alacrity", "Desolation", "Destruction", "Insight", "Malice", "Strength"],
+        seal:["Clarity", "Defense", "Knowledge", "Resilience", "Vigor", "Vitality"],
+        glyph:["Clarity", "Focus", "Force", "Knowledge", "Potency", "Shielding", "Warding"],
+        quint:["Alacrity", "Avarice", "Desolation", "Force", "Fortitude", "Insight", "Knowledge", "Malice", "Potency", "Strength", "Swiftness", "Vigor"]
+        };
+});
