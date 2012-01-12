7377c797e05fe46b6adaf8604413d6a386c0d86b
Russell Carlin <recarlin@fullsail.com>
2012-01-11 20:10:33 -0500

    Finished dynamic selector
    Finally figured out how to make my selector change based on what radio
    option was checked.

--- JS/main.js
+++ JS/main.js
@@ -1,33 +1,66 @@
 // Russell Carlin
 // VFW 1/12
 // Project 2
-window.addEventListener("DOMContentLoaded"), function (){
-    function ge(id){
-        var e = document.getElementById(id);
-        return e;
-    };
-    function runeSelection(){
-        var runeType = 
-    };
-    function showRunes(){
-        
-    };
-    function clearRunes(){
-        
-    };
-    function addRunes(){
-        
-    };
-    var sr = ge("show");
-    sr.addEventListener("click", showRunes);
-    var cr = ge("clear");
-    cr.addEventListener("click", clearRunes);
-    var ar = ge("add");
-    ar.addEventListener("click", addRunes);
+window.addEventListener("DOMContentLoaded", function () {
     var runes = {
         mark: ["Alacrity", "Desolation", "Destruction", "Insight", "Malice", "Strength"],
-        seal:["Clarity", "Defense", "Knowledge", "Resilience", "Vigor", "Vitality"],
-        glyph:["Clarity", "Focus", "Force", "Knowledge", "Potency", "Shielding", "Warding"],
-        quint:["Alacrity", "Avarice", "Desolation", "Force", "Fortitude", "Insight", "Knowledge", "Malice", "Potency", "Strength", "Swiftness", "Vigor"]
+        seal: ["Clarity", "Defense", "Knowledge", "Resilience", "Vigor", "Vitality"],
+        glyph: ["Clarity", "Focus", "Force", "Knowledge", "Potency", "Shielding", "Warding"],
+        quint: ["Alacrity", "Avarice", "Desolation", "Force", "Fortitude", "Insight", "Knowledge", "Malice", "Potency", "Strength", "Swiftness", "Vigor"]
+    };
+    function ge(id) {
+        var e = document.getElementById(id);
+        return e;
+    }
+    function getRuneType() {
+            var t = document.forms[0].type;
+            for (var i=0, l=t.length; i<l; i++) {
+                if(t[i].checked) {
+                    runeType = t[i].value;
+                };
+            };
         };
+    function runeSelection() {
+        if(runeType === "Mark"){
+            popSelect(runes.mark);
+        } else {
+            if(runeType === "Seal"){
+                popSelect(runes.seal);
+            } else {
+                if(runeType === "Glyph") {
+                    popSelect(runes.glyph);
+                } else {
+                    if(runeType === "Quintessence"){
+                        popSelect(runes.quint);
+                    };
+                };
+            };
+        };
+        function popSelect(x) {
+            var rs = ge("rname");
+            rs.options.length = 0
+            for(index in x) {
+                rs.options[rs.options.length] = new Option(x[index], index);
+            }
+        }
+    };
+    //function showRunes(){
+    //    
+    //};
+    //function clearRunes(){
+    //    
+    //};
+    //function addRunes(){
+    //    
+    //};
+    var pn = ge("type"),
+        runeType;
+    pn.addEventListener("click", getRuneType)
+    pn.addEventListener("click", runeSelection)
+    //var sr = ge("show");
+    //sr.addEventListener("click", showRunes);
+    //var cr = ge("clear");
+    //cr.addEventListener("click", clearRunes);
+    //var ar = ge("add");
+    //ar.addEventListener("click", addRunes);
 });
