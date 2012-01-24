fc0fc9f545afceb74324dc4288f997b34bc9a2a3
Russell Carlin <recarlin@fullsail.com>
2012-01-24 18:58:17 -0500

    Added JSON Data

---
+++ JS/json.js
@@ -0,0 +1,34 @@
+var filler = {
+    "runeSet1": {
+        "sec": ["Section Title:", "My Marks"],
+        "cat": ["Rune Category:", "Mark"],
+        "type": ["Rune Type:", "Destruction"],
+        "amount": ["Amount:", "9"],
+        "explain": ["Explaination:", "Gives my build more damage."],
+        "date": ["Date Added:", "2012-01-24"]
+    },
+    "runeSet2": {
+        "sec": ["Section Title:", "My Seals"],
+        "cat": ["Rune Category:", "Seal"],
+        "type": ["Rune Type:", "Resilence"],
+        "amount": ["Amount:", "9"],
+        "explain": ["Explaination:", "Makes me a durable against attacks early on."],
+        "date": ["Date Added:", "2012-01-24"]
+    },
+    "runeSet3": {
+        "sec": ["Section Title:", "My Glyphs"],
+        "cat": ["Rune Category:", "Glyph"],
+        "type": ["Rune Type:", "Warding"],
+        "amount": ["Amount:", "9"],
+        "explain": ["Explaination:", "Protects me from spells."],
+        "date": ["Date Added:", "2012-01-24"]
+    },
+    "runeSet4": {
+        "sec": ["Section Title:", "My Quints"],
+        "cat": ["Rune Category:", "Quintessence"],
+        "type": ["Rune Type:", "Strength"],
+        "amount": ["Amount:", "3"],
+        "explain": ["Explaination:", "Adds some flat damage to my attacks."],
+        "date": ["Date Added:", "2012-01-24"]
+    }
+}
